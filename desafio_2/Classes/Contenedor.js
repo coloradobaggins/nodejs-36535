236977435c16ea9eeb1458215c23e75f1f6e826a
colo <bugio89@gmail.com>
2022-06-15 19:31:18 -0300

    save(), getAll(), deleteAll()

--- desafio_2/Classes/Contenedor.js
+++ desafio_2/Classes/Contenedor.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { parse } = require('path');
 const { json } = require('stream/consumers');
 
 module.exports = class Contenedor{
@@ -18,62 +19,71 @@ module.exports = class Contenedor{
      * 
      * @param {obj} obj 
      */
-    async save(obj){
+    async save(obj){        
+        
+        if(await this.__checkFileEmpty()){
+            this.__writeFile();                                                         //Si esta completamente vacio, que le ponga los [] - Evito error de json.
+        }
+        
+        let allProducts = [];
+        let jsonData = await this.getAll();
 
-        console.log(`test from contenedor save()`);
-
-        console.log(obj.id);
+        let lastAssignedId = 0;
 
         
+        if(jsonData.length > 0){                                                        //BUscar el id mas alto, y recuper los objetos
 
-        //Leer el archivo...
-        let fileReaded = await this.readTheFile();
+            lastAssignedId = Math.max(...jsonData.map((products)=> products.id));
 
-        console.log(fileReaded);
+            jsonData.map((p)=>{
+
+                allProducts.push(p);
+                
+            });
+
+            console.log(`lastAssignedId: ${lastAssignedId}`);
+
+        }
+
+        obj.id = lastAssignedId + 1;
+
+        allProducts.push(obj);
+
+        console.log(`all productos:`);
+        console.log(allProducts);
+
         
-        /*
+        
+
+        //return jsonData;
+        //return readedFile;
+
+            
         try{
 
-            await fs.promises.appendFile(this.fileName, '['+JSON.stringify(obj, null, 2)+']');
+            //await fs.promises.writeFile(this.fileName, JSON.stringify(allProducts, null, 2)); 
+
+            this.__writeFile(JSON.stringify(allProducts, null, 2));
+
             console.log('se agrego texto al archivo');
 
         }catch(err){
+
             console.log(`Se produjo un error..`);
             console.log(err);
+
         }
-        */
+
+        
+
+        
+        
 
         //return id asignado
     }
 
+ 
 
-    async readTheFile(){
-
-        console.log('** reading file... **');
-
-        let parsedData;
-
-        try{
-
-            const fileData = await fs.promises.readFile(this.fileName, 'utf-8');
-            
-            
-
-            parsedData =  JSON.parse(fileData);
-
-            return parsedData;
-            //console.log(parsedData);
-            //console.log(parsedData.length);
-
-
-        }catch(err){
-
-            console.log('Error al leer archivo');
-            console.log(err);
-
-        }
-
-    }
 
     getById(id){
 
@@ -104,14 +114,65 @@ module.exports = class Contenedor{
     
     }
 
+
+    
+
+    
+
+    /**
+     * 
+     * TODO:: chequear si es un obj, si tengo [] o si esta vacio?
+     * 
+     * @returns json object
+     */
     async getAll(){
 
         try{
 
-            const fileData = await fs.promises.readFile(this.fileName, 'utf-8');
-            let jsonData = JSON.parse(fileData);
+            let readedFile = await this.__readFile();
+            
+            let jsonData = await JSON.parse(readedFile);
 
             return jsonData;
+
+        }catch(err){
+
+            console.log(`getAll() ...Error al obtener todos...`);
+            console.log(err);
+
+        }
+
+    }
+
+    /**
+     * Eliminar todos los objetos presentes en el archivo.
+     */
+    async deleteAll(){
+
+        try{
+
+            this.__writeFile();
+
+            return true;
+
+        }catch(err){
+
+            console.log(`Error al leer/escribir archivo`);
+            console.log(err);
+
+        }
+
+    }
+
+
+
+    async __readFile(){
+
+        try{
+
+            const fileData = await fs.promises.readFile(this.fileName, 'utf-8');
+
+            return fileData;
 
         }catch(err){
 
@@ -122,5 +183,60 @@ module.exports = class Contenedor{
 
     }
 
+    async __checkFileEmpty(){
+
+        let theFile = await this.__readFile();
+
+        
+            if (theFile.length === 0) {
+                //Code to be executed if the file is empty
+
+               return true;
+
+            }
+        return false;
+    }
+
+    async __writeFile(objToWrite = []){
+
+        let writeInFile = (objToWrite.length===0) ? '[]' : objToWrite;
+
+        try{
+
+            await fs.promises.writeFile(this.fileName, writeInFile)
+
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+
+
+
+
+
+
+
+    /********* */
+    //HELPER....no esta en uso...
+    __checkValidJson(fileData){
+
+        if (/^[\],:{}\s]*$/.test(fileData.replace(/\\["\\\/bfnrtu]/g, '@').
+        replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+        replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+
+        //the json is ok
+
+        console.log(`json valid`);
+
+        }else{
+
+            console.log(`json NOT valid`);
+
+        //the json is not ok
+
+        }
+
+    }
 
 }
--- desafio_2/app.js
+++ desafio_2/app.js
@@ -1,33 +1,63 @@
+const { send } = require('process');
 let contenedor = require('./Classes/Contenedor');
 
 //Test
 
 let productTest = {
     
-    title: 'Mouse',
-    price: 4250.99,
+    title: 'teclado',
+    price: 4222.99,
     thumbnail: 'https://cdn4.iconfinder.com/data/icons/electronics-and-gadget-3/64/MOUSE-devices-electronics-gadget-tools-256.png'
 } 
 
 let objContenedor = new contenedor('productos.txt');
 
+
+//console.log(JSON.stringify(productTest));
+
+
+
+
+/**************
+ *  Get by Id
+ */
 /*
+//objContenedor.getById(1);
 
-console.log(`***`);
-console.log(JSON.stringify(productTest));
-console.log(`***`); 
-objContenedor.save(productTest);
 
+/**************
+ *  Save
+ * ** OK **
+ */
+/*
+objContenedor.save(productTest)
+.then((res) => console.log(res))
+.catch((err) => console.log(err))
 */
 
+/**************
+ *  Get All
+ * ** OK **
+ */
 /*
 let getAllObjPromise = objContenedor.getAll();
 getAllObjPromise.then((res) => console.log(res));
 
 */
-console.log('******');
 
+/**************
+ *  Delete all
+ *  ** OK **
+ */
+/*
+objContenedor.deleteAll()
+.then((res)=>{
+    console.log(`Enviando a borrar..`);
+    console.log(res);
+})
+.catch((err)=>{
 
-objContenedor.getById(1);
+    console.log(err);
 
-
+});
+*/
