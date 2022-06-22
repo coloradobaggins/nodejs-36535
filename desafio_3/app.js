486c7a1cc7af3bd70297783ff09925bb9ddd085a
colo <bugio89@gmail.com>
2022-06-22 19:45:13 -0300

    Cambia puerto/config para deploy en glitch

--- desafio_3/app.js
+++ desafio_3/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Contenedor = require('./Classes/Contenedor.js');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const objContenedor = new Contenedor('productos.txt');
 
