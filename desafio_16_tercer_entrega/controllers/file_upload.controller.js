2357f1333095d9e78c594e8d045891c47194a17a
coloradobaggins <bugio89@gmail.com>
2022-08-18 15:08:25 -0300

    Actualiza vista perfil al cambiar imagen

--- desafio_16_tercer_entrega/controllers/file_upload.controller.js
+++ desafio_16_tercer_entrega/controllers/file_upload.controller.js
@@ -4,9 +4,6 @@ const userService = new UserService();
 
 let postFileUpload = async (req, res, next)=>{
 
-    console.log(`Subiendo foto..`);
-
-
     const file = req.file;
 
     if(!file){
@@ -15,18 +12,15 @@ let postFileUpload = async (req, res, next)=>{
         return next(error)
     }
 
-    console.log(`Guardar img en db`);
-
     const imgName = req.file.filename
-
-    console.log(`filename: ${imgName}`);
-
 
     let updateImgProfileResult = await userService.updateProfileImg(req.user._id, imgName);
 
+    let updated = (updateImgProfileResult!==null) ? true : false;
+
     res.json({
-        message: `Successfully uploaded files`,
-        updateImg: updateImgProfileResult
+        message: `Img uploaded`,
+        updated: updated
     });
 
 }
--- desafio_16_tercer_entrega/public/js/profile.js
+++ desafio_16_tercer_entrega/public/js/profile.js
@@ -3,6 +3,7 @@ console.log(`profile.js`);
 const form = document.getElementById('profile_img');
 form.addEventListener('submit', submitForm);
 
+/*
 function submitForm(e){
 
     e.preventDefault();
@@ -17,15 +18,50 @@ function submitForm(e){
     fetch('http://localhost:3001/file_upload', {
 
         method: 'POST',
-        body: formData/*,
-        headers: {
-            'Content-Type': 'multipart/form-data boundary="test"'
-        }*/
+        body: formData//,
+        //headers: {
+            //'Content-Type': 'multipart/form-data boundary="test"'
+        //}
         
 
     })
-    .then((result) =>{ console.log(result) })
+    .then((result) =>{ 
+        console.log(result) 
+
+        if(result.updated){
+            alert(`Reload`);
+            location.reload();
+        }
+
+    })
     .catch((err)=> { console.log(err) });
 
+
+}
+*/
+
+async function submitForm(e){
+
+    e.preventDefault();
+
+    const files = document.querySelector('#files');
+
+    const formData = new FormData();
+
+    formData.append('files', files.files[0]);
+
+    const rawResponse = await fetch('http://localhost:3001/file_upload', {
+
+        method: 'POST',
+        body: formData
+        
+    })
+
+    const content = await rawResponse.json();
+    console.log(content);
+    
+    if(content.updated){
+        location.reload();
+    }
 
 }
